6acb48a5699ab74a056aa3eccdbde9628ead1b99
hamzamindtech <hamosmn@gmail.com>
2020-10-07 22:20:50 +0000

    click button changes

--- src/EightComponent.js
+++ src/EightComponent.js
@@ -4,15 +4,18 @@ import './EightComponent.css';
 function EightComponent (){
 
     return (
+        
         <div class="contact-button">
-
+            <center>
             <p class="bottombutton" >
                 Contact the author for more on Real Madrid Transfer News</p>
             
             <a class="clickbutton" target="_blank" href="https://twitter.com/HamzaMindTECH">
                 CLICK HERE!
             </a>
+            </center>
         </div>
+        
     );
 }
 
